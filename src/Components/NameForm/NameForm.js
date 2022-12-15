f4595a02ee9b7d2829f5eb17c013a1021a0bc14a
DioguinhoSantos <diiogosantos@live.com>
2022-12-15 16:17:18 -0300

    commit1512

--- src/Components/NameForm/NameForm.js
+++ src/Components/NameForm/NameForm.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Form, Input} from '../MainPage/styles'
 
-const NameForm = (props) => {
+const NameForm = ({name, onChangeName, age, onChangeAge, email, onChangeEmail, confirmaEmail, onChangeConfirmaEmail, sendData}) => {
+
   return (
     <Form>
         <label>
           Nome:
-          <Input  placeholder="Nome" value={name} onChange={onChangeName}/>
+          <Input  placeholder="Nome" value={name} onChange={onChangeName} required/>
         </label>
+
         <label>
           Idade:
-          <Input  placeholder="Idade" value={age} onChange={onChangeAge}/>
+          <Input  placeholder="Idade" value={age} onChange={onChangeAge} type='number' required/>
         </label>
+
         <label>
           E-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail} type='email' required/>
         </label>
+
         <label>
           Confirmação de e-mail:
-          <Input  placeholder="usuario@usuario.com" value={email} onChange={onChangeEmail}/>
+          <Input  placeholder="usuario@usuario.com" value={confirmaEmail} onChange={onChangeConfirmaEmail} type='email' required/>
         </label>
+
       <button onClick={sendData}>Enviar dados</button>
       </Form>
   )
